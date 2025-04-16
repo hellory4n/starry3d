b7b98a72d39f53b8cf80fea062d7958fbc9ee0ce
hellory4n <hellory4n@gmail.com>
2025-04-15 19:51:11 -0500

    ui :)

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -1,8 +1,54 @@
-#include <stdio.h>
 #include <libtrippin.h>
 #include <st3d.h>
 #include <st3d_render.h>
 #include <st3d_ui.h>
+
+// used for the sliders :D
+static float st_pos_x;
+static float st_pos_y;
+static float st_pos_z;
+
+static float st_rot_x;
+static float st_rot_y;
+static float st_rot_z;
+
+static void camera_ui(void) {
+	struct nk_context* ctx = st3d_nkctx();
+
+	if (nk_begin(ctx, "camera test", nk_rect(25, 350, 300, 250),
+	NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_TITLE)) {
+		nk_layout_row_dynamic(ctx, 20, 1);
+		nk_label(ctx, "position", NK_TEXT_ALIGN_LEFT);
+
+		nk_layout_row_dynamic(ctx, 20, 2);
+		nk_label(ctx, "x:", NK_TEXT_ALIGN_LEFT);
+		nk_slider_float(ctx, -10, &st_pos_x, 10, 0.01);
+
+		nk_layout_row_dynamic(ctx, 20, 2);
+		nk_label(ctx, "y:", NK_TEXT_ALIGN_LEFT);
+		nk_slider_float(ctx, -10, &st_pos_y, 10, 0.01);
+
+		nk_layout_row_dynamic(ctx, 20, 2);
+		nk_label(ctx, "z:", NK_TEXT_ALIGN_LEFT);
+		nk_slider_float(ctx, -10, &st_pos_z, 10, 0.01);
+
+		nk_layout_row_dynamic(ctx, 20, 1);
+		nk_label(ctx, "rotation", NK_TEXT_ALIGN_LEFT);
+
+		nk_layout_row_dynamic(ctx, 20, 2);
+		nk_label(ctx, "x:", NK_TEXT_ALIGN_LEFT);
+		nk_slider_float(ctx, 0, &st_rot_x, 360, 0.01);
+
+		nk_layout_row_dynamic(ctx, 20, 2);
+		nk_label(ctx, "y:", NK_TEXT_ALIGN_LEFT);
+		nk_slider_float(ctx, 0, &st_rot_y, 360, 0.01);
+
+		nk_layout_row_dynamic(ctx, 20, 2);
+		nk_label(ctx, "z:", NK_TEXT_ALIGN_LEFT);
+		nk_slider_float(ctx, 0, &st_rot_z, 360, 0.01);
+	}
+	nk_end(ctx);
+}
 
 int main(void)
 {
@@ -43,42 +89,7 @@ int main(void)
 
 		// nuklear calls go inside here
 		st3d_ui_begin();
-		struct nk_colorf bg = {0.10f, 0.18f, 0.24f, 1.0f};
-		struct nk_context* ctx = st3d_nkctx();
-		if (nk_begin(ctx, "Demo", nk_rect(50, 50, 230, 250),
-		NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
-		NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
-		{
-			enum {EASY, HARD};
-			static int op = EASY;
-			static int property = 20;
-			nk_layout_row_static(ctx, 30, 80, 1);
-			if (nk_button_label(ctx, "button"))
-				fprintf(stdout, "button pressed\n");
-
-			nk_layout_row_dynamic(ctx, 30, 2);
-			if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-			if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-
-			nk_layout_row_dynamic(ctx, 25, 1);
-			nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-			nk_layout_row_dynamic(ctx, 20, 1);
-			nk_label(ctx, "background:", NK_TEXT_LEFT);
-			nk_layout_row_dynamic(ctx, 25, 1);
-			if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx),400))) {
-				nk_layout_row_dynamic(ctx, 120, 1);
-				bg = nk_color_picker(ctx, bg, NK_RGBA);
-				nk_layout_row_dynamic(ctx, 25, 1);
-				bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f,0.005f);
-				bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f,0.005f);
-				bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f,0.005f);
-				bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f,0.005f);
-				nk_combo_end(ctx);
-			}
-		}
-
-		nk_end(ctx);
+		camera_ui();
 		st3d_ui_end();
 
 		st3d_end_drawing();
