6c0958fe21585fb7755f27995652157dcf9a7527
hellory4n <hellory4n@gmail.com>
2025-08-14 15:27:22 -0400

    what?

--- sandbox/src/debug_mode.h
+++ sandbox/src/debug_mode.h
@@ -15,14 +15,16 @@ static inline void debug_mode()
 	ImGui::Text("FPS: %f", st::fps());
 
 	st::Camera& cam = st::Camera::current();
-	ImGui::Text("Camera: (%s)", cam.projection == st::CameraProjection::PERSPECTIVE
-					    ? "perspective"
-					    : "orthographic");
+	ImGui::Text(
+		"Camera: (%s)",
+		cam.projection == st::CameraProjection::PERSPECTIVE ? "perspective" : "orthographic"
+	);
 	ImGui::BulletText("Position: %f, %f, %f", cam.position.x, cam.position.y, cam.position.z);
 	ImGui::BulletText("Rotation: %f, %f, %f", cam.rotation.x, cam.rotation.y, cam.rotation.z);
-	ImGui::BulletText("%s: %f",
-			  cam.projection == st::CameraProjection::PERSPECTIVE ? "FOV" : "Zoom",
-			  cam.fov); // it's a union, the value is the same but with different names
+	ImGui::BulletText(
+		"%s: %f", cam.projection == st::CameraProjection::PERSPECTIVE ? "FOV" : "Zoom",
+		cam.fov // it's a union, the value is the same but with different names
+	);
 
 	ImGui::End();
 }
