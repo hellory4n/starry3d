576a8cf77d9adb8c24e2b96f20cc83b5e46862f0
hellory4n <hellory4n@gmail.com>
2025-12-27 21:18:09 -0400

    fuck.

--- starry/render.zig
+++ starry/render.zig
@@ -1,5 +1,6 @@
 //! This renderer is called Emerson Victor. Be nice to Emerson Victor.
 const std = @import("std");
+const zglm = @import("zglm");
 const sg = @import("sokol").gfx;
 const app = @import("app.zig");
 const log = @import("log.zig").stlog;
@@ -30,29 +31,85 @@ pub fn __deinit() void {
     log.info("deinitialized renderer", .{});
 }
 
+pub fn __draw() void {
+    sg.applyPipeline(global.pipeline);
+    sg.draw(0, 6, 1);
+}
+
 fn softwareRenderer() !void {
     const framebuffer = try std.fs.cwd().createFile("out.ppm", .{ .read = true });
     defer framebuffer.close();
+    var w = framebuffer.writer(&.{});
+    const writer = &w.interface;
 
     const fb_width = 640;
     const fb_height = 360;
 
     // ppm header
-    var shia_labuffer: [64]u8 = undefined;
-    try framebuffer.writeAll("P6\n");
-    const crap = try std.fmt.bufPrint(&shia_labuffer, "{d} {d}\n", .{ fb_width, fb_height });
-    try framebuffer.writeAll(crap);
-    try framebuffer.writeAll("255\n");
-    try framebuffer.sync();
+    _ = try writer.write("P6\n");
+    try writer.print("{d} {d}\n", .{ fb_width, fb_height });
+    _ = try writer.write("255\n");
+    _ = try writer.flush();
 
     // i am writing it
     // this is horribly slow :)
-    inline for (0..fb_width) |_| for (0..fb_height) |_| {
-        try framebuffer.writeAll(&[_]u8{ 255, 0, 0 });
+
+    const start = std.time.milliTimestamp();
+
+    inline for (0..fb_height) |y| for (0..fb_width) |x| {
+        const u = @as(f32, @floatFromInt(x)) / (fb_width - 1);
+        const v = @as(f32, @floatFromInt(y)) / (fb_height - 1);
+
+        const color = pixel(
+            zglm.vec2f(u, v),
+            zglm.vec2f(@floatFromInt(fb_width), @floatFromInt(fb_height)),
+            zglm.Vec3f.zero(),
+        );
+
+        // convert to 8 bit color
+        // alpha is ignored fuck you
+        const r: u8 = @intFromFloat(color.r() * 255);
+        const g: u8 = @intFromFloat(color.g() * 255);
+        const b: u8 = @intFromFloat(color.b() * 255);
+
+        _ = try writer.write(&[_]u8{ r, g, b });
     };
+
+    const end = std.time.milliTimestamp();
+    std.log.debug("render took {d} ms", .{end - start});
 }
 
-pub fn __draw() void {
-    sg.applyPipeline(global.pipeline);
-    sg.draw(0, 6, 1);
+// actual rendering stuff
+
+const Ray = struct {
+    origin: zglm.Vec3f,
+    dir: zglm.Vec3f,
+
+    pub fn at(ray: Ray, t: f32) zglm.Vec3f {
+        return ray.origin.add(ray.dir.muls(t));
+    }
+
+    pub fn color(_: Ray) zglm.Vec4f {
+        return zglm.Vec4f.zero();
+    }
+};
+
+/// simulates what the final shader would look like
+fn pixel(
+    texcoord: zglm.Vec2f,
+    _: zglm.Vec2f,
+    _: zglm.Vec3f,
+) zglm.Vec4f {
+    // fuckery
+    // const aspect_ratio = resolution.width() / resolution.height();
+
+    // const image_width: i32 = @intFromFloat(resolution.width());
+    // const image_height: i32 = @max(@as(i32, @intFromFloat(resolution.width() / aspect_ratio)), 1);
+
+    // // camera
+    // const focal_length: f32 = 1;
+    // const viewport_height: f32 = 2.0;
+    // const viewport_width = viewport_height * (@as(f32, @floatFromInt(image_width)) / image_height);
+
+    return zglm.swizzle(texcoord, .xy01);
 }
